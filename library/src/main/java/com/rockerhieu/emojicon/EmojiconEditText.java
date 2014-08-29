adfdc4b7b89674bace238825cca06d44ce095918
Wit Kurowski <wit@tictocplanet.com>
2014-08-29 13:29:36 -0700

    Removed uses of the "this" keyword.

--- library/src/main/java/com/rockerhieu/emojicon/EmojiconEditText.java
+++ library/src/main/java/com/rockerhieu/emojicon/EmojiconEditText.java
@@ -54,7 +54,7 @@ public class EmojiconEditText extends EditText {
 
     @Override
     protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
-        this.updateText();
+        updateText();
     }
 
     /**
@@ -63,9 +63,9 @@ public class EmojiconEditText extends EditText {
     public void setEmojiconSize(int pixels) {
         mEmojiconSize = pixels;
 
-        this.updateText();
+        updateText();
     }
-    
+
     private void updateText() {
         EmojiconHandler.addEmojis(getContext(), getText(), mEmojiconSize, mUseSystemDefault);
     }
